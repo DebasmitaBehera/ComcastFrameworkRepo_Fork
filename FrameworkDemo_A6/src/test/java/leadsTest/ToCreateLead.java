857ae863013e25c15b58797bafecfc6085bfd032
Soumyabrata Nandi <Soumyabrata Nandi@192.168.137.137>
2024-11-05 18:14:23 +0530

    commitmsg

--- FrameworkDemo_A6/src/test/java/leadsTest/ToCreateLead.java
+++ FrameworkDemo_A6/src/test/java/leadsTest/ToCreateLead.java
@@ -4,7 +4,8 @@ public class ToCreateLead {
 
 	public static void main(String[] args) {
 
-		System.out.println("Leads created by me");
+		System.out.println("Leads created by me for practice");
+		System.out.println("--------------------------");
 	}
 
 }
